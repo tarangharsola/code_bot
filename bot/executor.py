2da08bad5973a1de341eb406b4d75df0414aabb3
tarangharsola <tarang.harsola@gmail.com>
2025-12-30 22:21:38 +0530

    lol

--- bot/executor.py
+++ bot/executor.py
@@ -1,32 +1,48 @@
+
 import subprocess
-from bot.utils import git_commit
 import os
+from bot.utils import git_commit
+from bot.config import load_config
+
+
+def clone_target_repo():
+  config = load_config()
+  repo_url = f"https://x-access-token:{os.environ.get('GITHUB_TOKEN','')}@github.com/{config['target_repo']}.git"
+  if not os.path.exists("target_repo"):
+    subprocess.run(["git", "clone", repo_url, "target_repo"], check=True)
 
 def execute_plan(plan, state):
+  clone_target_repo()
+  cwd = os.getcwd()
+  os.chdir("target_repo")
+  try:
     for step in plan:
-        action = step['action']
-        # Each action is mapped to a function
-        if action == "add_html_structure":
-            add_html_structure()
-        elif action == "add_base_css":
-            add_base_css()
-        elif action == "add_favicon_robots":
-            add_favicon_robots()
-        elif action == "add_layout_system":
-            add_layout_system()
-        elif action == "add_typography":
-            add_typography()
-        elif action == "add_header":
-            add_header()
-        elif action == "refine_section":
-            refine_section()
-        elif action == "polish_micro":
-            polish_micro()
-        elif action == "accessibility":
-            accessibility()
-        else:
-            raise Exception(f"Unknown action: {action}")
-        git_commit(step['description'])
+      action = step['action']
+      # Each action is mapped to a function
+      if action == "add_html_structure":
+        add_html_structure()
+      elif action == "add_base_css":
+        add_base_css()
+      elif action == "add_favicon_robots":
+        add_favicon_robots()
+      elif action == "add_layout_system":
+        add_layout_system()
+      elif action == "add_typography":
+        add_typography()
+      elif action == "add_header":
+        add_header()
+      elif action == "refine_section":
+        refine_section()
+      elif action == "polish_micro":
+        polish_micro()
+      elif action == "accessibility":
+        accessibility()
+      else:
+        raise Exception(f"Unknown action: {action}")
+      git_commit(step['description'])
+    subprocess.run(["git", "push"], check=True)
+  finally:
+    os.chdir(cwd)
 
 def add_html_structure():
     # Write a minimal, semantic index.html
