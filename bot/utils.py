6650b795c4465a838a510b0e4b063a293c786b7b
tarangharsola <tarang.harsola@gmail.com>
2026-01-04 14:07:31 +0530

    commit

--- bot/utils.py
+++ bot/utils.py
@@ -2,4 +2,7 @@ import subprocess
 
 def git_commit(message):
     subprocess.run(["git", "add", "."], check=True)
-    subprocess.run(["git", "commit", "-m", message], check=True)
+    # Only commit if there are staged changes
+    result = subprocess.run(["git", "diff", "--cached", "--quiet"])
+    if result.returncode == 1:
+        subprocess.run(["git", "commit", "-m", message], check=True)
