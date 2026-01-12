e5bc8e21ecb46cc5385e3bfec17c6e54cdb74488
tarangharsola <tarang.harsola@gmail.com>
2026-01-12 15:59:05 +0530

    olkolkj

--- bot/utils.py
+++ bot/utils.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def git_commit(message):
+    # Set user details for commit attribution
+    subprocess.run(["git", "config", "user.name", "tarangharsola"], check=True)
+    subprocess.run(["git", "config", "user.email", "tarang.harsola@gmail.com"], check=True)
     subprocess.run(["git", "add", "."], check=True)
     # Only commit if there are staged changes
     result = subprocess.run(["git", "diff", "--cached", "--quiet"])
